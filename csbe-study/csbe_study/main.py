4dfc35d7ff625e9124d6177a352e5792c3f031dc
teo-jang <torisdream@gmail.com>
2025-06-10 17:47:23 +0900

    --amend

--- csbe-study/csbe_study/main.py
+++ csbe-study/csbe_study/main.py
@@ -2,14 +2,14 @@ import asyncio
 
 from fastapi import FastAPI
 
-from csbe_study.routers import uploader, data
+# from routers import uploader, data
 from routers import printer
 
 app = FastAPI()
 
 app.include_router(printer.router)
-app.include_router(uploader.router)
-app.include_router(data.router)
+# app.include_router(uploader.router)
+# app.include_router(data.router)
 
 
 @app.get("/")
